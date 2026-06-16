c7097a6fdc00a425d2f807d532854dc6a40046bd
Ricky Hu <ricky9667@gmail.com>
2026-06-16 12:11:24 +0800

    Format project configuration files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,37 @@
 export default {
-  content: ['./index.html', './public/**/*.html', './src/**/*.{ts,vue}'],
+  content: ["./index.html", "./public/**/*.html", "./src/**/*.{ts,vue}"],
   theme: {
     extend: {
       screens: {
-        'xs': '576px'
+        xs: "576px",
       },
       spacing: {
-        '84': '21rem',
-        '96': '24rem',
-        '108': '27rem',
-        '120': '30rem',
-        '132': '33rem',
-        '144': '36rem',
-        '156': '39rem',
-        '168': '42rem'
+        "84": "21rem",
+        "96": "24rem",
+        "108": "27rem",
+        "120": "30rem",
+        "132": "33rem",
+        "144": "36rem",
+        "156": "39rem",
+        "168": "42rem",
       },
       colors: {
-        primary: '#335443',
-        secondary: '#e5edd3',
-        title: '#3e3a39',
-        content: '#595757'
-      }
+        primary: "#335443",
+        secondary: "#e5edd3",
+        title: "#3e3a39",
+        content: "#595757",
+      },
     },
     minHeight: {
-      '0': '0',
-      '1/4': '25%',
-      '1/2': '50%',
-      '3/4': '75%',
-      'full': '100%',
-    }
+      "0": "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
